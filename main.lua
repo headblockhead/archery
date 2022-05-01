5521f288318dc40c3b93bf2aab53c25114a9a42e
Headblockhead <headblockhead@gmail.com>
2022-05-01 17:31:56 +0100

    Added save code (WIP) and menu items

--- main.lua
+++ main.lua
@@ -4,8 +4,9 @@ import "CoreLibs/sprites"
 import "CoreLibs/timer"
 import "CoreLibs/animation"
 import "CoreLibs/easing"
+import "saver"
 -- GFX as a useful shorthand for the playdate's graphics.
-local gfx<const> = playdate.graphics
+local gfx <const> = playdate.graphics
 
 -- Define fonts that will be used.
 local ubuntu_mono = gfx.font.new("fonts/ubuntuMONOreg")
@@ -29,7 +30,7 @@ TNT_enemy_1_1:setTag(1)
 local level1 = {
 	enemies = { TNT_enemy_1_1 },
 	walls = {},
-	cannonballs = 3,
+	cannonballs = 5,
 }
 
 -- LEVEL 2
@@ -41,7 +42,7 @@ TNT_enemy_2_1:setTag(1)
 local level2 = {
 	enemies = { TNT_enemy_2_1 },
 	walls = {},
-	cannonballs = 3,
+	cannonballs = 5,
 }
 
 -- LEVEL 3
@@ -53,10 +54,44 @@ TNT_enemy_3_1:setTag(1)
 local level3 = {
 	enemies = { TNT_enemy_3_1 },
 	walls = {},
-	cannonballs = 3,
+	cannonballs = 5,
 }
 
-levels = { level1, level2, level3 }
+-- LEVEL 4
+local TNT_enemy_4_1 = gfx.sprite.new(Box_image)
+TNT_enemy_4_1:moveTo(280, 208)
+TNT_enemy_4_1:setCollideRect(0, 0, TNT_enemy_4_1:getSize())
+TNT_enemy_4_1:setTag(1)
+
+local wall_enemy_4_2 = gfx.sprite.new(Wall_image)
+wall_enemy_4_2:moveTo(180, 170)
+wall_enemy_4_2:setCollideRect(0, 0, wall_enemy_4_2:getSize())
+wall_enemy_4_2:setTag(2)
+
+local level4 = {
+	enemies = { TNT_enemy_4_1 },
+	walls = { wall_enemy_4_2 },
+	cannonballs = 5,
+}
+
+-- LEVEL 5
+local TNT_enemy_5_1 = gfx.sprite.new(TNT_image)
+TNT_enemy_5_1:moveTo(280, 208)
+TNT_enemy_5_1:setCollideRect(0, 0, TNT_enemy_5_1:getSize())
+TNT_enemy_5_1:setTag(1)
+
+local wall_enemy_5_2 = gfx.sprite.new(Wall_image)
+wall_enemy_5_2:moveTo(180, 170)
+wall_enemy_5_2:setCollideRect(0, 0, wall_enemy_5_2:getSize())
+wall_enemy_5_2:setTag(2)
+
+local level5 = {
+	enemies = { TNT_enemy_5_1 },
+	walls = { wall_enemy_5_2 },
+	cannonballs = 5,
+}
+
+levels = { level1, level2, level3, level4, level5 }
 
 
 -- Define sound FX players
@@ -69,6 +104,28 @@ clickSFX = playdate.sound.sampleplayer.new("SFX/tick")
 --Setup the crank
 playdate.setCrankSoundsDisabled(true)
 
+
+-- Debug for loading long-digit levels
+local buffer = 0
+local bufferindex = 1
+function playdate.keyPressed(key)
+	if (not key:find("%D")) then
+		buffer = buffer + (tonumber(key) / bufferindex)
+		bufferindex = bufferindex * 10
+	end
+	if (key == "P") then
+		level = tonumber(buffer * (bufferindex / 10))
+		used_cannonballs = 0
+		inticks = 0
+		outticks = 0
+		defeated_enemies = 0
+		debug_load_level(tonumber(buffer * (bufferindex / 10)))
+		updateballs(used_cannonballs, level_cannonball_limit)
+		buffer = 0
+		bufferindex = 1
+	end
+end
+
 function debug_load_level(lvl)
 	-- Clear the playfield for the new level
 	for _, level in ipairs(levels) do
@@ -76,25 +133,6 @@ function debug_load_level(lvl)
 		playdate.graphics.sprite.removeSprites(level.walls)
 	end
 	-- Load the NEW level
-	current_level = levels[lvl]
-	for _, enemy in ipairs(current_level.enemies) do
-		enemy:add()
-	end
-	for _, wall in ipairs(current_level.walls) do
-		wall:add()
-	end
-	level_cannonball_limit = current_level.cannonballs
-	level_enimies_count = #current_level.enemies
-end
-
-function load_level(lvl)
-	-- Clear the playfield for the next level
-	if (lvl > 1) then
-		prev_level = levels[lvl - 1]
-		playdate.graphics.sprite.removeSprites(prev_level.enemies)
-		playdate.graphics.sprite.removeSprites(prev_level.walls)
-	end
-	-- Load the next level
 	current_level = levels[lvl]
 	for _, enemy in ipairs(current_level.enemies) do
 		enemy:add()
@@ -121,11 +159,11 @@ local sprite_arc = gfx.sprite.new(arc_image)
 sprite_arc:moveTo(30, 210)
 sprite_arc:add()
 
-local image_ball = gfx.image.new("images/ball")
-local sprite_ball = gfx.sprite.new(image_ball)
-sprite_ball:moveTo(10, 230)
-sprite_ball:setCollideRect(0, 0, sprite_ball:getSize())
-sprite_ball:add()
+local image_arrow = gfx.image.new("images/ball")
+local sprite_arrow = gfx.sprite.new(image_arrow)
+sprite_arrow:moveTo(10, 230)
+sprite_arrow:setCollideRect(0, 0, sprite_arrow:getSize())
+sprite_arrow:add()
 
 local aim_image = gfx.image.new(400, 240)
 
@@ -212,10 +250,10 @@ assert(background_image)
 
 gfx.sprite.setBackgroundDrawingCallback(
 	function(x, y, width, height)
-		gfx.setClipRect(x, y, width, height)
-		background_image:draw(0, 0)
-		gfx.clearClipRect()
-	end
+	gfx.setClipRect(x, y, width, height)
+	background_image:draw(0, 0)
+	gfx.clearClipRect()
+end
 )
 
 local MAX_VELOCITY = 8.0 -- The fastest the ball can be set to go.
@@ -240,6 +278,7 @@ function updatevelocity(velocity)
 	gfx.popContext()
 
 	meter_line_sprite:setImage(new_velocity_image)
+	last_aim_velocity = velocity
 end
 
 function updateballs(used, max)
@@ -271,7 +310,7 @@ function updateaim(angle)
 
 	local x = 240 * math.tan(math.rad(angle))
 
-	sprite_ball:setRotation(angle)
+	sprite_arrow:setRotation(angle)
 
 	new_aim_image = gfx.image.new(400, 240)
 
@@ -284,6 +323,25 @@ function updateaim(angle)
 	aim_sprite:setImage(new_aim_image)
 
 	last_aim_angle = angle
+end
+
+function load_level(lvl)
+	-- Clear the playfield for the next level
+	if (lvl > 1) then
+		prev_level = levels[lvl - 1]
+		playdate.graphics.sprite.removeSprites(prev_level.enemies)
+		playdate.graphics.sprite.removeSprites(prev_level.walls)
+	end
+	-- Load the next level
+	current_level = levels[lvl]
+	for _, enemy in ipairs(current_level.enemies) do
+		enemy:add()
+	end
+	for _, wall in ipairs(current_level.walls) do
+		wall:add()
+	end
+	level_cannonball_limit = current_level.cannonballs
+	level_enimies_count = #current_level.enemies
 end
 
 function calc_movement(velocity, angle)
@@ -319,27 +377,30 @@ local projectile_path = {}
 -- Level.
 local level = 1
 
--- Debug for loading long-digit levels
-local buffer = 0
-local bufferindex = 1
-function playdate.keyPressed(key)
-	if (not key:find("%D")) then
-		buffer = buffer + (tonumber(key) / bufferindex)
-		bufferindex = bufferindex * 10
-	end
-	if (key == "P") then
-		print(buffer * (bufferindex / 10))
-		level = tonumber(buffer * (bufferindex / 10))
-		used_cannonballs = 0
-		inticks = 0
-		outticks = 0
-		defeated_enemies = 0
-		debug_load_level(tonumber(buffer * (bufferindex / 10)))
-		updateballs(used_cannonballs, level_cannonball_limit)
-		buffer = 0
-		bufferindex = 1
-	end
-end
+--Add settings to menu
+local menu = playdate.getSystemMenu()
+
+local menuItem, error = menu:addMenuItem("Save now", function()
+	save()
+end)
+
+local checkmarkMenuItem, error = menu:addCheckmarkMenuItem("Auto Save", true, function(value)
+	saveauto(value)
+end)
+
+local menuItem2, error = menu:addMenuItem("Title", function()
+	save()
+	used_cannonballs = 0
+	inticks = 0
+	outticks = 0
+	defeated_enemies = 0
+	print("Open Title")
+	change_state(STATE_TITLE)
+end)
+
+-- Add image to menu side
+menu_image_bg = gfx.image.new("images/menu_bg")
+playdate.setMenuImage(menu_image_bg)
 
 -- Run on every frame
 function playdate.update()
@@ -378,13 +439,13 @@ function playdate.update()
 	end
 
 	if (state == STATE_FIRING) then
-		for _, overlapping_sprite in pairs(sprite_ball:overlappingSprites()) do
+		for _, overlapping_sprite in pairs(sprite_arrow:overlappingSprites()) do
 			if (overlapping_sprite:getTag() == 1) then
 				explosionSFX:play()
 				overlapping_sprite:remove()
 				defeated_enemies = defeated_enemies + 1
 			elseif (overlapping_sprite:getTag() == 2) then
-				sprite_ball.y = 280
+				sprite_arrow.y = 280
 			end
 		end
 
@@ -394,36 +455,37 @@ function playdate.update()
 		local x, y = calc_movement(velocity, 90.0 - angle)
 		y = y - gravity * (ticks / 2.0)
 
-		sprite_ball:moveTo(sprite_ball.x + x, sprite_ball.y - y)
+		sprite_arrow:moveTo(sprite_arrow.x + x, sprite_arrow.y - y)
 		start = { x = 0, y = 240 }
-		table.insert(projectile_path, { x = sprite_ball.x + x, y = sprite_ball.y + y })
+		table.insert(projectile_path, { x = sprite_arrow.x + x, y = sprite_arrow.y + y })
 		if #projectile_path > 20 then
 			start = projectile_path[1]
 			table.remove(projectile_path, 1)
 		end
 		-- s=o/h c=a/h t=o/a
 
-		xx = sprite_ball.x - start.x
-		yy = sprite_ball.y - start.y
+		xx = sprite_arrow.x - start.x
+		yy = sprite_arrow.y - start.y
 		projectile_angle = (math.deg(math.atan(yy, xx) + 90)) % 360
 		-- The arrow should not be facing backwards. If it is, face it straight down.
-		if (projectile_angle > 180) then
+		if (projectile_angle > 180 and projectile_angle < 340) then
 			projectile_angle = 180
 		end
-		sprite_ball:setRotation(projectile_angle)
-		sprite_ball:setCollideRect(0, 0, sprite_ball:getSize())
+		sprite_arrow:setRotation(projectile_angle)
+		sprite_arrow:setCollideRect(0, 0, sprite_arrow:getSize())
 
 		-- If the ball has gone below the screen.
-		if (sprite_ball.y > 250) then
+		if (sprite_arrow.y > 250) then
 			used_cannonballs = used_cannonballs + 1
-			sprite_ball:setRotation(90)
-			sprite_ball:moveTo(10, 230)
+			sprite_arrow:setRotation(90)
+			sprite_arrow:moveTo(10, 230)
 			ticks = 0
 			velocity = 0.0
 
 			if (used_cannonballs <= level_cannonball_limit and defeated_enemies >= level_enimies_count) then
 				-- If all of the enimies have been defeated. (and within the cannonball limit)
 				level = level + 1
+				print(level)
 				used_cannonballs = 0
 				inticks = 0
 				outticks = 0
@@ -455,7 +517,7 @@ function playdate.update()
 			-- Load the level
 			load_level(level)
 			updateballs(used_cannonballs, level_cannonball_limit)
-			sprite_ball:setRotation(90)
+			sprite_arrow:setRotation(90)
 			inticks = inticks + 1
 		elseif (outticks < 40) then
 			outticks = outticks + 1
@@ -472,7 +534,10 @@ function playdate.update()
 end
 
 function change_state(new_state)
+	print("State was: " .. state)
 	state = new_state
+	print("State changed to: " .. new_state)
+	print("State is: " .. state)
 	gfx.sprite.update()
 	playdate.timer.updateTimers()
 end
---
+++ saver.lua
@@ -0,0 +1,13 @@
+-- import "CoreLibs/object"
+-- import "CoreLibs/graphics"
+-- import "CoreLibs/sprites"
+-- import "CoreLibs/timer"
+-- import "CoreLibs/animation"
+-- import "CoreLibs/easing"
+function save()
+    print("Saving...")
+end
+
+function saveauto(value)
+    print("Autosave: ", value)
+end
